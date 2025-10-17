0b94ccb684888a9b7dc0c2bee280195a2cd13539
Agustín Brandan <agustinbrandan@MacBook-Air-de-Agustin.local>
2025-10-17 04:50:40 -0300

    Configure Vercel rewrites for Express app

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,9 @@
-import app from "../packages/server/src/app";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import app from "../packages/server/src/app.js";
 
-export default app;
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  if (req.url) {
+    req.url = req.url.replace(/^\/api/, "") || "/";
+  }
+  return app(req, res);
+}
